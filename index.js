3726e64c73a29008308d026f9fb4120a033070b8
mattkim <matthewjkim@gmail.com>
2016-03-03 15:48:26 -0500

    separate instagram function

--- index.js
+++ index.js
@@ -2,8 +2,20 @@
     var token = $('#token');
     var album = $('#album');
 
+    function findTagFromInstagram(tag) {
+      $.ajax({
+        url: 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=ce95cb4e56c146c994457b48a839f6a8',
+        dataType: 'jsonp',
+        success: function(result) {
+          for (var i = 0; i < result.data.length; i++) {
+            var url = result.data[i].images.thumbnail.url;
+            $('#instagramdetails').append('<img src="' + url + '"/>');
+          }
+        }
+      });
+    };
+
     $('#submit').on('click', function() {
-      console.log('in here');
       $.ajax({
         url: "https://api.spotify.com/v1/browse/new-releases",
         dataType: 'json',
@@ -12,22 +24,10 @@
           limit: 1
         },
         success: function(data, status) {
-
           var iframe = '<iframe class="spotifylink" src="https://embed.spotify.com/?uri=' + data.albums.items[0].uri + '&" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
           album.html(iframe);
-
-          var albumName = data.albums.items[0].name.replace(/[^A-Z0-9]/ig, '');
-          alert(albumName);
-          $.ajax({
-            url: 'https://api.instagram.com/v1/tags/' + albumName + '/media/recent?client_id=ce95cb4e56c146c994457b48a839f6a8',
-            dataType: 'jsonp',
-            success: function(result) {
-              for (var i = 0; i < result.data.length; i++) {
-                var url = result.data[i].images.thumbnail.url;
-                $('#instagramdetails').append('<img src="' + url + '"/>');
-              }
-            }
-          });
+          var albumName = data.albums.items[0].name.replace(/[^A-Z0-9]/ig, '');          
+          findTagFromInstagram(albumName);
         },
         error: function(data) {
           alert(data.responseJSON.error.message);
